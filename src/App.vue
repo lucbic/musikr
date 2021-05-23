109ec92b7e9eb36cba88d1359c962b2cc2389d17
Lucas Bicudo <lucas.bicudo@convenia.com.br>
2021-05-23 15:37:48 -0300

    App barebones

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+div(id="app")
+  intervals-container
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IntervalsContainer from './containers/Intervals'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IntervalsContainer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: $base-text-color
+  width: 100vw
+  height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+
 </style>
---
+++ src/containers/Intervals.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div(class="bacon")
+  p teste loko creise
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+.bacon
+  height: 500px
+  width: 500px
+  display: flex
+  justify-content: center
+  align-items
+  background-color: red
+</style>
