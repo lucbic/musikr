0cfef588ab75c8a3557a83ac6ac42e9e1cdbf3bf
Lucas Bicudo <lucas.bicudo@convenia.com.br>
2021-05-23 19:40:29 -0300

    Add tabs component

---
+++ src/components/Tabs.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div(class="tab-selector")
+  base-button(
+    class="btn"
+    v-for="(val, key) in tabs"
+    :key="key"
+    :highlighted="selected == key"
+  ) {{ val }}
+
+</template>
+
+<script>
+import BaseButton from './Button'
+
+export default {
+  components: { BaseButton },
+
+  props: {
+    tabs: {
+      type: Object,
+      default: () => {}
+    },
+    selected: [ String, Number ]
+  }
+
+}
+</script>
+
+<style lang="sass">
+.tab-selector
+  display: flex
+  margin: 0 -10px
+
+  > .btn
+    margin: 0 10px
+
+</style>
