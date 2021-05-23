5dad87124de429327947b47310110eed9ef59894
Lucas Bicudo <lucas.bicudo@convenia.com.br>
2021-05-23 19:40:36 -0300

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+button( :class="[ 'base-button' ,{ '-highlighted': highlighted } ]")
+  h4
+    slot
+</template>
+
+<script>
+export default {
+  props: {
+    highlighted: Boolean
+  }
+
+}
+</script>
+
+<style lang="sass">
+.base-button
+  height: 40px
+  display: flex
+  align-items: center
+  justify-content: center
+  border: $base-border
+  border-radius: $base-radius
+  background-color: transparent
+  color: $base-color
+  background-repeat: no-repeat
+  cursor: pointer
+  outline: none
+  padding: 0 40px
+
+  &.-highlighted
+    background-color: $base-color
+    > h4
+      color: $base-white
+
+</style>
