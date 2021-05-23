31116cab49e48b76bef227c22989b77aa81bd0a1
Lucas Bicudo <lucas.bicudo@convenia.com.br>
2021-05-23 19:40:47 -0300

    Improve flashcard

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,23 +1,62 @@
 <template lang="pug">
-div(class="flash-card")
-  h1(class="note") {{ note }}
+div(:class="[ 'flash-card', classes ]")
+  component(:is="textTag" class="note") {{ note }}
 </template>
 
 <script>
 export default {
   props: {
-    note: String
+    note: String,
+    size: {
+      type: String,
+      default: 'small',
+      validator: s => ['small', 'large'].includes(s)
+    },
+    status: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    textTag () {
+      return {
+        'small': 'h3',
+        'large': 'h1'
+      }[this.size] || 'h1'
+    },
+
+    classes () {
+      return {
+        '-large': this.size === 'large',
+        '-error': this.status === 'error',
+        '-success': this.status === 'success',
+      }
+    }
   }
 }
 </script>
 
 <style lang="sass">
 .flash-card
-  height: 200px
-  width: 160px
+  height: 100px
+  width: 80px
   display: flex
   justify-content: center
   align-items: center
   border-radius: $base-radius
   border: $base-border
+  cursor: pointer
+  user-select: none
+  flex-shrink: 0
+
+  &.-large
+    height: 200px
+    width: 160px
+  
+  &.-error
+    background-color: $error-color
+
+  &.-success
+    background-color: $success-color
 </style>
